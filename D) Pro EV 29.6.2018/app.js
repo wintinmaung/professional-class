b88adb8c773ff613aa7db5950ddbb43cedd642cb
wintinmaung <wintinmaung250@gmail.com>
2018-08-20 13:16:29 +0630

    first commit

---
+++ D) Pro EV 29.6.2018/app.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+//   alert("Hello World!");
+//   $("#blackBox").hide();
+// });
+
+// $(document).ready(function(){
+//     $("h2").click(function(){
+//         $(this).hide();
+//     });
+// });
+
+$(document).ready(function(){
+	$("h2").click(function(){
+		$(this).hide();	
+	});	
+	$("h2").click(function(){
+		alert("Hello World");
+	 $("#blackBox").hide();
+	});
+ 
+});
---
+++ D) Pro EV 29.6.2018/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  alert("Hello World!");
+  $("#blackBox").hide();
+});
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
